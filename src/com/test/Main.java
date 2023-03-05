f11a40f84a1078e9d73a24e0d8797214c161a35b
yanqi17118357 <3224848830@qq.com>
2023-03-05 20:36:03 +0800

    特殊包装类

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,14 +1,16 @@
 package com.test;
 
-
-import com.test.entity.Status;
-import com.test.entity.Student;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setStatus(Status.RUNNING);
-        System.out.println(student.getStatus().getName());
+        BigDecimal i = BigDecimal.valueOf(10);
+        i = i.divide(BigDecimal.valueOf(3), 100, RoundingMode.DOWN);
+        //计算10/3的结果，精确到小数点后100位
+        //RoundingMode是舍入模式，就是精确到最后一位时，该怎么处理，这里CEILING表示向上取整，FLOOR表示向下取整
+        System.out.println(i);
     }
 }
