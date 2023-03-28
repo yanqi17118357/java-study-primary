300024d654dd48b9166cb43916b0ea46c118da9e
yanqi17118357 <3224848830@qq.com>
2023-03-28 21:56:25 +0800

    集合类与IO实战：图书管理系统

--- src/com/test/Book.java
+++ src/com/test/Book.java
@@ -1,11 +1,25 @@
 package com.test;
 
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable {
     private String title;
     private String author;
     private int price;
 
-    private Book(String title, String author, int price) {
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Book(String title, String author, int price) {
         this.title = title;
         this.author = author;
         this.price = price;
@@ -13,43 +27,8 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", price=" + price +
-                '}';
+        return "《"+title+"》作者："+author+"（"+price+"￥）";
     }
 
-    //建造者模式
-    public static BookBuilder builder() {
-        return new BookBuilder();
-    }
 
-    public static class BookBuilder {
-        private String title;
-        private String author;
-        private int price;
-
-        private BookBuilder() {
-        }
-
-        public BookBuilder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public BookBuilder author(String author) {
-            this.author = author;
-            return this;
-        }
-
-        public BookBuilder price(int price) {
-            this.price = price;
-            return this;
-        }
-
-        public Book build() {
-            return new Book(title, author, price);
-        }
-    }
 }
--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,5 +1,8 @@
 package com.test;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,9 +10,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final List<Book> LIST = new LinkedList<>();
+    private static List<Book> LIST;
 
     public static void main(String[] args) {
+        System.out.println("正在初始化图书管理系统...");
+        load();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("=====================图书管理系统=====================");
@@ -24,24 +29,106 @@ public class Main {
                 case 1:
                     insert(scanner);
                     break;
+                case 2:
+                    query();
+                    break;
+                case 3:
+                    delete(scanner);
+                    break;
+                case 4:
+                    modify(scanner);
+                    break;
                 case 5:
-                    System.out.println("拜拜");
+                    System.out.println("正在保存图书数据...");
+                    save();
+                    System.out.println("感谢使用，拜拜！");
                     return;
             }
         }
     }
 
+    @SuppressWarnings("unchecked")
+    private static void load() {
+        File file = new File("data.txt");
+        if (file.exists()) {
+            try (ObjectInputStream stream = new ObjectInputStream(Files.newInputStream(Paths.get("data.txt")))) {
+                LIST = (List<Book>) stream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            LIST = new LinkedList<>();
+        }
+    }
+
+    private static void save() {
+        try (ObjectOutputStream stream = new ObjectOutputStream(Files.newOutputStream(Paths.get("data.txt")))) {
+            stream.writeObject(LIST);
+            stream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
     private static void insert(Scanner scanner) {
+        // 由于nextInt()方法不会获取回车键，所以这里和下面都要消耗一下
         scanner.nextLine();
-        Book book = Book.builder()
-                .title(scanner.nextLine())
-                .author(scanner.nextLine())
-                .price(scanner.nextInt())
-                .build();
+        System.out.println("请输入书籍名称");
+        String name = scanner.nextLine();
+        System.out.println("请输入作者名称");
+        String author = scanner.nextLine();
+        System.out.println("请输入书籍价格");
+        int price = scanner.nextInt();
+        Book book = new Book(name, author, price);
+        // 同上
         scanner.nextLine();
 
         LIST.add(book);
-        System.out.println(LIST);
+        System.out.println("书籍添加成功：" + book);
+    }
+
+    private static void query() {
+        for (int i = 0; i < LIST.size(); i++) {
+            System.out.println(i + "." + LIST.get(i));
+        }
+    }
+
+    private static void modify(Scanner scanner) {
+        scanner.nextLine();
+        System.out.println("请输入你要修改的书籍编号");
+        int index = scanner.nextInt();
+        scanner.nextLine();
+        while (index > LIST.size() - 1 || index < 0) {
+            System.out.println("书籍标号不合法，请重新输入");
+            index = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        Book book = LIST.get(index);
+        System.out.println("请输入书籍名称");
+        book.setTitle(scanner.nextLine());
+        System.out.println("请输入作者名称");
+        book.setAuthor(scanner.nextLine());
+        System.out.println("请输入书籍价格");
+        book.setPrice(scanner.nextInt());
+        scanner.nextLine();
+
+    }
+
+    private static void delete(Scanner scanner) {
+        scanner.nextLine();
+        System.out.println("请输入你要删除的书籍编号");
+        int index = scanner.nextInt();
+        scanner.nextLine();
+        while (index > LIST.size() - 1 || index < 0) {
+            System.out.println("书籍标号不合法，请重新输入");
+            index = scanner.nextInt();
+            scanner.nextLine();
+        }
+        LIST.remove(index);
+        System.out.println("删除书籍信息成功！");
 
     }
 }
