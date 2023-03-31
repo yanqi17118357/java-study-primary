1029344b83eb9897310109108ad9763ce16f0dda
yanqi17118357 <3224848830@qq.com>
2023-03-31 23:10:26 +0800

    守护线程

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,45 +1,24 @@
 package com.test;
 
+
 public class Main {
-    private static int i = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        Main main = new Main();
-        Main main2 = new Main();
-        new Thread(() -> {
-            for (int j = 0; j < 100000; j++) {
-                synchronized (main) {
-                    System.out.println("这里是线程1");
-                    try {
-                        Thread.sleep(1000);
-                        synchronized (main2) {
-                            System.out.println("调用main锁");
-                        }
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-
+        Thread main = Thread.currentThread();
+        Thread t = new Thread(() -> {
+            while (true) {
+                System.out.println("我是守护线程");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
-        }).start();
+        });
 
-        new Thread(() -> {
-            for (int j = 0; j < 100000; j++) {
-                synchronized (main2) {
-                    System.out.println("这里是线程2");
-                    try {
-                        Thread.sleep(1000);
-                        synchronized (main) {
-                            System.out.println("这里是main锁");
-                        }
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        }).start();
+        t.setDaemon(true);
+        t.start();
 
-        System.out.println("这里是主线程");
-
+        Thread.sleep(3000);
     }
 }
