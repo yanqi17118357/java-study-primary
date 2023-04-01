eb78f49c2e971c668f0bba7e0440c7c88f06fda6
yanqi17118357 <3224848830@qq.com>
2023-04-01 14:13:45 +0800

    生产者和消费者

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,26 +1,50 @@
 package com.test;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        List<Integer> list = new ArrayList<>();
-        new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
-                list.add(i);
-            }
-        }).start();
+    private static Queue<Object> queue = new LinkedList<>();
 
-        new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
-                list.add(i);
-            }
-        }).start();
+    public static void main(String[] args) {
+        new Thread(Main::add, "厨师1").start();
+        new Thread(Main::add, "厨师2").start();
 
-        Thread.sleep(1000);
-        System.out.println(list.size());
+        new Thread(Main::take, "消费者1").start();
+        new Thread(Main::take, "消费者2").start();
+        new Thread(Main::take, "消费者3").start();
+
+    }
+    private static void add() {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+                synchronized (queue) {
+                    String name = Thread.currentThread().getName();
+                    System.out.println(new Date() + " " + name + "出餐了！");
+                    queue.offer(new Object());
+                    queue.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    private static void take() {
+        while (true) {
+            try {
+                synchronized (queue) {
+                    while (queue.isEmpty()) queue.wait();
+                    queue.poll();
+                    String name = Thread.currentThread().getName();
+                    System.out.println(new Date() + " " + name + "拿到了餐品，正在享用！");
+                }
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
