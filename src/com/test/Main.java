4513e50601acc181f4e9a1602c81a61f131ad68b
yanqi17118357 <3224848830@qq.com>
2023-04-01 16:02:06 +0800

    类加载器

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,21 +1,35 @@
 package com.test;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws ReflectiveOperationException {
-        Integer i = new Integer(10);
 
-        Field value = Integer.class.getDeclaredField("value");
-        value.setAccessible(true);
+    //定义一个自己的ClassLoader
+    static class MyClassLoader extends ClassLoader{
+        public Class<?> defineClass(String name, byte[] b){
+            return defineClass(name, b, 0, b.length);   //调用protected方法，支持载入外部class文件
+        }
+    }
 
-        value.set(i, 19);
-        value.set(i, 20);
-        System.out.println(value.get(i));
+    public static void main(String[] args) throws IOException, ReflectiveOperationException{
+        MyClassLoader classLoader = new MyClassLoader();
+        FileInputStream stream = new FileInputStream("Test.class");
+        byte[] bytes = new byte[stream.available()];
+        stream.read(bytes);
+        Class<?> clazz = classLoader.defineClass("com.test.Test", bytes);   //类名必须和我们定义的保持一致
+        Object o = clazz.newInstance();
 
+        Method method = clazz.getDeclaredMethod("test");
+        method.setAccessible(true);
+
+        method.invoke(o);
 
     }
 }
