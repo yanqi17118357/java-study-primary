c4ea8e236313c1c18b7c57fbf326437aa78daa1d
yanqi17118357 <3224848830@qq.com>
2023-04-01 14:40:51 +0800

    Class类

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,50 +1,17 @@
 package com.test;
 
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
-    private static Queue<Object> queue = new LinkedList<>();
+    public static void main(String[] args) throws ClassNotFoundException {
+        //通过包名
+        Class<?> clazz = Class.forName("com.test.Student");
+        //通过new一个对象
+        Class<?> clazz2 = new Student().getClass();
+        //直接类.class
+        Class<?> clazz3 = Student.class;
 
-    public static void main(String[] args) {
-        new Thread(Main::add, "厨师1").start();
-        new Thread(Main::add, "厨师2").start();
+        Class<?> c = int.class;
 
-        new Thread(Main::take, "消费者1").start();
-        new Thread(Main::take, "消费者2").start();
-        new Thread(Main::take, "消费者3").start();
-
-    }
-    private static void add() {
-        while (true) {
-            try {
-                Thread.sleep(3000);
-                synchronized (queue) {
-                    String name = Thread.currentThread().getName();
-                    System.out.println(new Date() + " " + name + "出餐了！");
-                    queue.offer(new Object());
-                    queue.notifyAll();
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    private static void take() {
-        while (true) {
-            try {
-                synchronized (queue) {
-                    while (queue.isEmpty()) queue.wait();
-                    queue.poll();
-                    String name = Thread.currentThread().getName();
-                    System.out.println(new Date() + " " + name + "拿到了餐品，正在享用！");
-                }
-                Thread.sleep(4000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
---
+++ src/com/test/Student.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Student {
+    String name;
+    int age;
+
+    public void test() {
+
+    }
+}
